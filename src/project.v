6e878fae58b24f643df796417dc5330e9be0f313
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-20 13:42:20 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_Sai222777 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,31 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,uio_in,1'b0};
+  
+  assign uio_out = 0;
+  assign uio_oe = 0;
+  
+  dual_xor_stream_cipher #( .M(32) ) uut  (
+    .clk(clk),
+    .rst(!rst_n),
+    .sel0(ui_in[0]),
+    .sel1(ui_in[1]),
+    .tx_p(ui_in[2]),
+    .rx_e(ui_in[3]),
+    .cfg_en(ui_in[4]),
+    .cfg_i(ui_in[5]),
+    .tx_en(ui_in[6]),
+    .rx_en(ui_in[7]),
+    
+    .tx_e(uo_out[0]),
+    .rx_p(uo_out[1]),
+    .dbg_tx_p(uo_out[2]),
+    .dbg_rx_e(uo_out[3]), 
 
+    .cfg_o(uo_out[4]),
+    .heartbeat(uo_out[7:5])
+    );
+  
 endmodule
